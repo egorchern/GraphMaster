e42882a09085e9ec6b6c265c34648a2fb557c179
Egor Chernyshev <egorch.formal@gmail.com>
2021-01-08 01:24:35 +0000

    created coordinate resolution

--- src/app.tsx
+++ src/app.tsx
@@ -3,16 +3,89 @@ import { render } from "react-dom";
 
 let root = document.querySelector("#root");
 let canvas, ctx;
-let node_raidus = 30;
-let x_distance_between_nodes = 40;
+let node_raidus = 27;
+let x_distance_between_nodes;
 let node_edge_angle = 150;
 let edge_width = 1.5;
 const pi = Math.PI;
 
+//convert degrees to radians
 function to_radians(degrees){
     return degrees * (pi / 180);
 }
 
+// return true position of mouse on canvas;
+function getMousePos(evt, canv) {
+    
+    var rect = canv.getBoundingClientRect();
+    return {
+        x: (evt.clientX - rect.left) / (rect.right - rect.left) * canv.width,
+        y: (evt.clientY - rect.top) / (rect.bottom - rect.top) * canv.height
+    };
+}
+
+// resolves x any y coordinates based on angle from positive x-axis. -Pi to Pi range
+function find_x_and_y(radius, start_x, start_y, angle) {
+    
+    let x;
+    let y;
+    // if in right top quadrant
+    if (angle >= -pi / 2 && angle < 0) {
+        angle = Math.abs(angle);
+        x = Math.cos(angle) * radius + start_x;
+        y = start_y + (radius - Math.sin(angle) * radius);
+    }
+    // if in right bottom quadrant
+    else if (angle >= 0 && angle <= pi / 2) {
+        x = Math.cos(angle) * radius + start_x;
+        y = start_y + (Math.sin(angle) * radius + radius);
+    }
+    // if in left top quadrant
+    else if (angle < -pi / 2 && angle >= -pi) {
+        angle = Math.abs(angle);
+        x = start_x + Math.cos(angle) * radius
+        
+        y = start_y + (radius - Math.sin(angle) * radius);
+
+
+    }
+    // if in left bottom quadrant
+    else if(angle > pi/2 && angle <= pi){
+        x = start_x + Math.cos(angle) * radius
+        y = start_y + (Math.sin(angle) * radius + radius);
+    }
+    
+    return [x, y];
+
+
+}
+/*
+function resolve_coordinates_by_angle(start_x, start_y, radius, angle){
+    if(angle === 90){
+        return {
+            x: start_x + radius,
+            y: start_y
+
+        }
+    }
+    let scoped_angle = Math.abs(90 - angle);
+    let temp = Math.sin(to_radians(scoped_angle)) * radius;
+    
+    let y_coord;
+    if(angle < 90){
+        y_coord = start_y - temp;
+    }
+    else{
+        y_coord = start_y + temp;
+    }
+    let x_coord = Math.sqrt(Math.pow(radius, 2) - Math.pow(temp, 2));
+    
+    return {
+        x: x_coord,
+        y: y_coord
+    }
+}
+*/
 //gives random integer between min(inclusive) and max(inclusive)
 function get_random_int(min, max) {
     min = Math.ceil(min);
@@ -30,17 +103,34 @@ class Node{
         this.name = name;
     }
     draw(){
-        ctx.clearRect(0, 0, canvas.width, canvas.height);
+       
         ctx.beginPath();
         ctx.save();
+        // draw node circle
         ctx.fillStyle = "hsl(0, 0%, 10%)"
         ctx.arc(this.center_x, this.center_y, node_raidus, 0, to_radians(360));
         ctx.fill();
+        // draw node name
         ctx.font = "40px Lato";
         ctx.textAlign = "center";
         ctx.fillStyle = "hsl(0, 0%, 100%)";
         ctx.textBaseline = "middle";
         ctx.fillText(this.name ,this.center_x /*- node_raidus * 0.50*/, this.center_y /*+ node_raidus * 0.50*/, node_raidus);
+    }
+}
+
+
+class Canvas_mouse_position_tracker extends React.Component{
+    constructor(props){
+        super(props);
+    }
+    render(){
+        return (
+            <div id="mouse_tracker">
+                <span>X: {this.props.x}</span>
+                <span>Y: {this.props.y}</span>
+            </div>
+        )
     }
 }
 
@@ -51,17 +141,58 @@ class Graph extends React.Component{
         super(props);
         this.graph = this.props.graph;
         this.node_list = [];
+        
     }
-    
+    populate_node_list(){
+        let node_names = Object.keys(this.graph);
+        
+        let n = node_names.length;
+        x_distance_between_nodes = 35 * n;
+        let center_x = canvas.width / 2;
+        let center_y = canvas.height / 2 - x_distance_between_nodes;
+        
+        let local_node_list = [];
+        
+        let angle_increment = 360 / n;
+        let angles = [0];
+        
+        for(let i = 0; i < n; i += 1){
+            let last_angle = angles[angles.length - 1];
+            angles.push(last_angle + angle_increment);
+        }
+        for(let i = 0; i < n; i += 1){
+            let angle = angles[i];
+            angle = to_radians(angle);
+            angle = angle - pi/2;
+            if (angle > pi){
+                angle -= 2 * pi;
+            }
+            let temp = find_x_and_y(x_distance_between_nodes, center_x, center_y, angle);
+            let x = temp[0];
+            let y = temp[1];
+            console.log(angle);
+            local_node_list.push(new Node(x, y, node_names[i]));
+        }
+        
+        
+        
+        
+        this.node_list = local_node_list;
+    }
+    draw_nodes(){
+        this.node_list.forEach(node => {
+            node.draw();
+        })
+    }
     componentDidMount(){
         canvas = document.querySelector("#canvas");
         ctx = canvas.getContext('2d');
-        let test_node = new Node(360, 360, "N");
-        test_node.draw();
+        this.populate_node_list();
+        this.draw_nodes();
     }
     render(){
         return(
-            <canvas id="canvas" width="1000" height="800">
+            <canvas id="canvas" width="1000" height="800" onMouseMove={this.props.onMouseMove}>
 
             </canvas>
         )
@@ -69,22 +200,50 @@ class Graph extends React.Component{
 }
 
 class App extends React.Component{
-    graph: { A: { B: number; C: number; }; B: { D: number; A: number; }; C: { A: number; D: number; F: number; G: number; }; D: { B: number; C: number; }; F: { C: number; }; G: { C: number; }; };
+    canvas_x: number;
+    canvas_y: number;
+   
     
     constructor(props){
         super(props);
-        this.graph = {
+        this.state = {
+            canvas_mouse_pos: {
+                x: 0,
+                y: 0
+            }
+        }
+        
+    }
+    on_canvas_mouse_move = (e) => {
+        let pos = getMousePos(e, canvas);
+        this.setState({
+            canvas_mouse_pos: {
+                x: pos.x,
+                y: pos.y
+            }
+        })
+    }
+    render(){
+        let graph = {
             "A": {"B": 4, "C": 12},
             "B": {"D": 13, "A": 4},
             "C": {"A": 12, "D": 8, "F": 14, "G": 15},
             "D": {"B": 13, "C": 8},
             "F": {"C": 14},
-            "G": {"C": 15}
+            "G": {"C": 15},
+            "GT":{},
+            "GTO": {},
+            "KTO" : {},
+            "GGG": {}
+            
+            
+            
         }
-    }
-    render(){
         return(
-            <Graph graph={this.graph}/>
+            <div>
+                <Canvas_mouse_position_tracker x={this.state.canvas_mouse_pos.x} y={this.state.canvas_mouse_pos.y}/>
+                <Graph graph={graph} onMouseMove={this.on_canvas_mouse_move}/>
+            </div>
         )
     }
 }
